ac6d39ac3fabbd2c590006712b74922fd280adf5
Bekasyl <bekasyl2007amanshaev@gmail.com>
2025-02-21 22:30:06 +0500

    Yesterday, Today, Tomorrow

--- LAB_4/date/task2.py
+++ LAB_4/date/task2.py
@@ -1,8 +1,8 @@
 from datetime import datetime, timedelta
 
-current_date = datetime.now()
-yesterday = current_date - timedelta(days=1)
-tomorrow = current_date + timedelta(days=1)
-print("Yesterday:", yesterday.date())
-print("Today:", current_date.date())
-print("Tomorrow:", tomorrow.date())
+now = datetime.now()
+yesterday = now - timedelta(days=1)
+tomorrow = now + timedelta(days=1)
+print(yesterday.date())
+print(now.date())
+print(tomorrow.date())
--- LAB_4/date/task3.py
+++ LAB_4/date/task3.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
 
 now = datetime.now()
-without_microseconds = now.replace(microsecond=0)
-print(without_microseconds)
+a = now.replace(microsecond=0)
+print(a)
